7d730f55f2255bb2eefa1ff08142af9b0c24df43
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2017-12-08 14:46:24 -0700

    ugh

---
+++ app-2.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  let submitbtn = $('#button3')
+  submitbtn.click(function() {
+  $.get("https://lit-fortress-6467.herokuapp.com/object", function(data) {
+    for (let i = 0; i < data.length; i++) {
+      let p = $('<p></p>')
+      p.text(data[i].title)
+      $('.results').append(p)
+      console.log(data[i])
+    }
+  })
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
--- app.js
+++ app.js
@@ -2,17 +2,14 @@ $(document).ready(function() {
 
 
 
+  $.get("https://lit-fortress-6467.herokuapp.com/object", function(data) {
+    for (let i = 0; i < data.length; i++) {
+      let newimg = $('<p></p>');
+      newimg.text(data[i].title)
 
-
-
-
-
-
-
-
-
-
-
+      console.log(data[i].title)
+    }
+  })
 
 
 
